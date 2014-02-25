ece294ebf663db11968965b391f1fac51446e57b
Nitix <guillaume.pierson.gp@gmail.com>
2014-02-25 09:50:33 +0100

    Ajout des modeles

--- Model/RestoModel.php
+++ Model/RestoModel.php
@@ -1,10 +1,51 @@
 <?php
 
+use Guzzle\Http\Client;
+
 /**
  * Description of RestoModel
  *
  * @author Guillaume
  */
 class RestoModel {
-    //put your code here
+
+    private $guzzleClient;
+    static private $baseUri = 'https://webetu/www/canals5/crazylunch/';
+    static private $ressourceName = "restos/";
+    private $arrayData;
+    private $rawdata;
+
+    public function __construct() {
+	$this->guzzleClient = new Client(static::$baseUri);
+    }
+
+    public function find($id) {
+	$request = $this->guzzleClient->get(static::$ressourceName . $id);
+	$response = $request->send();
+	$this->rawData = $response->json();
+	$this->arrayData = json_decode($this->rawdata);
+    }
+
+    public function findAll() {
+	$request = $this->guzzleClient->get(static::$ressourceName);
+	$response = $request->send();
+	$this->rawData = $response->json();
+	$this->arrayData = json_decode($this->rawdata);
+    }
+    
+
+    public function findRel($id, $rel) {
+	$request = $this->guzzleClient->get(static::$ressourceName . $id.'/'.$rel);
+	$response = $request->send();
+	$this->rawData = $response->json();
+	$this->arrayData = json_decode($this->rawdata);
+    }
+    
+    public function getJson() {
+	return $this->rawData;
+    }
+
+    public function getArray(){
+	return $this->arrayData;
+    }
 }
