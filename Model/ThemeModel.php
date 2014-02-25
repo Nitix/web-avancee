6c3c6e22df96b7f6811da70d8e6787623728f585
Krack54 <teragon54@hotmail.fr>
2014-02-25 09:17:36 +0100

    azert

--- Model/ThemeModel.php
+++ Model/ThemeModel.php
@@ -5,6 +5,22 @@
  *
  * @author Guillaume
  */
+use Guzzle\Http\Client;
+
 class ThemeModel {
-    //put your code here
+
+	private $guzzleClient;
+	private $baseUri = 'https://webetu.iutnc.univ-lorraine.fr/www/canals5/crazylunch/';
+	private $resourceName ='Themes'	;
+	private $arrayData = array();
+	private $rawData;
+
+
+	public function __construct(){
+		$this->guzzleClient = new Client(static::$baseUri);
+	}
+
+	public function find($id){
+		$this->guzzleClient->get();
+	}
 }
