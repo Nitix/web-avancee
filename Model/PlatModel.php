3c5135bb1dbfca0223157036777c623804c244ff
Nitix <guillaume.pierson.gp@gmail.com>
2014-03-18 09:27:12 +0100

    Indetation correct

--- Model/PlatModel.php
+++ Model/PlatModel.php
@@ -14,37 +14,37 @@ class PlatModel {
     private $rawData;
 
     public function __construct() {
-	$this->guzzleClient = new Client(static::$baseUri);
+        $this->guzzleClient = new Client(static::$baseUri);
     }
 
     public function find($id) {
-	$this->request = $this->guzzleClient->get(static::$ressourceName . $id);
-	$this->reponse = $request->send();
-	$this->rawData = $reponse->json();
-	$this->arrayData = json_decode($rawData);
+        $this->request = $this->guzzleClient->get(static::$ressourceName . $id);
+        $this->reponse = $request->send();
+        $this->rawData = $reponse->json();
+        $this->arrayData = json_decode($rawData);
     }
 
     public function findAll() {
-	$this->request = $this->guzzleClient->get(static::$ressourceName);
-	$this->reponse = $request->send();
-	$this->rawData = $reponse->json();
-	$this->arrayData = json_decode($rawData);
+        $this->request = $this->guzzleClient->get(static::$ressourceName);
+        $this->reponse = $request->send();
+        $this->rawData = $reponse->json();
+        $this->arrayData = json_decode($rawData);
     }
 
     public function findRel($id, $relation) {
 
-	$this->request = $this->guzzleClient->get(static::$ressourceName . $id . '/' . $relation);
-	$this->reponse = $request->send();
-	$this->rawData = $reponse->json();
-	$this->arrayData = json_decode($rawData);
+        $this->request = $this->guzzleClient->get(static::$ressourceName . $id . '/' . $relation);
+        $this->reponse = $request->send();
+        $this->rawData = $reponse->json();
+        $this->arrayData = json_decode($rawData);
     }
 
     public function getJson() {
-	return $this->rawData;
+        return $this->rawData;
     }
 
     public function getArray() {
-	return $this->arrayData;
+        return $this->arrayData;
     }
 
 }
--- Model/RestoModel.php
+++ Model/RestoModel.php
@@ -16,36 +16,36 @@ class RestoModel {
     private $rawdata;
 
     public function __construct() {
-	$this->guzzleClient = new Client(static::$baseUri);
+        $this->guzzleClient = new Client(static::$baseUri);
     }
 
     public function find($id) {
-	$request = $this->guzzleClient->get(static::$ressourceName . $id);
-	$response = $request->send();
-	$this->rawData = $response->json();
-	$this->arrayData = json_decode($this->rawdata);
+        $request = $this->guzzleClient->get(static::$ressourceName . $id);
+        $response = $request->send();
+        $this->rawData = $response->json();
+        $this->arrayData = json_decode($this->rawdata);
     }
 
     public function findAll() {
-	$request = $this->guzzleClient->get(static::$ressourceName);
-	$response = $request->send();
-	$this->rawData = $response->json();
-	$this->arrayData = json_decode($this->rawdata);
+        $request = $this->guzzleClient->get(static::$ressourceName);
+        $response = $request->send();
+        $this->rawData = $response->json();
+        $this->arrayData = json_decode($this->rawdata);
     }
 
     public function findRel($id, $rel) {
-	$request = $this->guzzleClient->get(static::$ressourceName . $id . '/' . $rel);
-	$response = $request->send();
-	$this->rawData = $response->json();
-	$this->arrayData = json_decode($this->rawdata);
+        $request = $this->guzzleClient->get(static::$ressourceName . $id . '/' . $rel);
+        $response = $request->send();
+        $this->rawData = $response->json();
+        $this->arrayData = json_decode($this->rawdata);
     }
 
     public function getJson() {
-	return $this->rawData;
+        return $this->rawData;
     }
 
     public function getArray() {
-	return $this->arrayData;
+        return $this->arrayData;
     }
 
 }
