5ef3699b013e54a553c6ab5b92a826091dc1ec6d
HakshoLOL <adri1du54@hotmail.fr>
2014-03-18 10:34:26 +0100

    $

--- Model/Vue.php
+++ Model/Vue.php
@@ -1,2 +1,14 @@
 <?php
 
+
+class Vue
+{
+
+	?><!DOCTYPE html>
+	<html lang="fr">
+		<head>
+			<meta charset="utf-8">
+			<title>Une faim</title>
+			<link rel="stylesheet" href="ressource/style.css"/>
+		</head>
+		<body>
