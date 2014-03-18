232af772156ee40ae3a82960781222eb685fcf1c
Nitix <guillaume.pierson.gp@gmail.com>
2014-03-18 11:08:43 +0100

    Corretion controlleur

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -1,14 +1,14 @@
 <?php
-	
+
 class Controller{
 
-	public function __construct(){
-		
-		$this->action=  array (
-						'lt' => 'listetheme',
-						't' => 'theme',
-						'lr' =>'listeresto',
-						'r' =>'resto'
+    public function __construct(){
+
+        $this->action=  array (
+            'lt' => 'listetheme',
+            't' => 'theme',
+            'lr' =>'listeresto',
+            'r' =>'resto'
 						'lp' => 'listeplat',
 						'p' => 'plat',
 						'ac' =>'ajoutpanier',
@@ -16,124 +16,134 @@ class Controller{
 						);
 	}
 
-public function echoJson(){
-	header("Content-type: application/json");
-}
+    public function echoJson(){
+        header("Content-type: application/json");
+    }
 
 
-public function callAction($param=null){
-		
-		if(isset($param["a"])) { // si $param contient
+    public function callAction($param=null){
 
-			if (array_key_exists($param["a"], $this->action)){
-				$a = $this->action[$param['a']];
-				return $this->$a($param);
+        if(isset($param["a"])) { // si $param contient
 
-			}else{
+            if (array_key_exists($param["a"], $this->action)){
+                $a = $this->action[$param['a']];
+                return $this->$a($param);
 
-				return $this->defaut();
-			}
-		}else{
+            }else{
 
-				return $this->defautPage();
-			}
-	}
+                return $this->defaut();
+            }
+        }else{
 
-	public function defautPage(){
-		header('Location: web-avancee?a=lt');      
-  					exit();
-	}
+            return $this->defautPage();
+        }
+    }
 
-	public function defaut(){
-		echo "404 page not found";
-	}
+    public function defautPage(){
+        header('Location: web-avancee?a=lt');
+        exit();
+    }
 
-
+    public function defaut(){
+        echo "404 page not found";
+    }
 
 
 
-public function listetheme(){
-	$r = new ThemeModel() ;
-	$json = $r->findAll()->getJson();
-	$this->echoJson();
-	echo $json;
-}	
+
+
+    public function listetheme(){
+        $r = new ThemeModel() ;
+        $json = $r->findAll()->getJson();
+        $this->returnJson();
+        echo $json;
+    }
 
 }
 
 public function theme($param){
-	if(!isset($param['id'])){
-		echo $this->defaut();
-	}else{
+    if(!isset($param['id'])){
+        echo $this->defaut();
+    }else{
 
-		$r = new ThemeModel() ;
-		$json = $r->find($param['id'])->getJson();
-		echo $json;
-}	
+        $r = new ThemeModel() ;
+        $json = $r->find($param['id'])->getJson();
+        $this->returnJson();
+        echo $json;
+    }
 }
 
 
 
 public function listeresto($param){
-	if(!isset($param['id'])){
-		echo $this->defaut();
-	}else{
-	
-	$r = new ThemeModel() ;
-	$json = $r->findRel($param['id'], 'restos')->getJson();
-	echo $json;
-}	
+    if(!isset($param['id'])){
+        echo $this->defaut();
+    }else{
+
+        $r = new ThemeModel() ;
+        $json = $r->findRel($param['id'], 'restos')->getJson();
+        $this->returnJson();
+        echo $json;
+    }
 
 }
 
 public function resto($param){
-	if(!isset($param['id'])){
-		echo $this->defaut();
-	}else{
-	$r = new RestoModel() ;
-	$json = $r->find($param['id'])->getJson();
-	echo $json;
-}	
+    if(!isset($param['id'])){
+        echo $this->defaut();
+    }else{
+        $r = new RestoModel() ;
+        $json = $r->find($param['id'])->getJson();
+        $this->returnJson();
+        echo $json;
+    }
 
 }
 
 public function listeplat($param){
-	if(!isset($param['id'])){
-		echo $this->defaut();
-	}else{
-	$r = new RestoModel() ;
-	$json = $r->find($param['id'])->getJson();
-	echo $json;	
-	
-}	
+    if(!isset($param['id'])){
+        echo $this->defaut();
+    }else{
+        $r = new RestoModel() ;
+        $json = $r->find($param['id'])->getJson();
+        $this->returnJson();
+        echo $json;
+
+    }
 
 }
 
 public function plat($param){
-	if(!isset($param['id'])){
-		echo $this->defaut();
-	}else{
-	$r = new PlatModel() ;
-	$json = $r->find($param['id'])->getJson();
-	echo $json;	
-	
-}	
+    if(!isset($param['id'])){
+        echo $this->defaut();
+    }else{
+        $r = new PlatModel() ;
+        $json = $r->find($param['id'])->getJson();
+        $this->returnJson();
+        echo $json;
+
+    }
 
 }
 
 public function ajoutpanier($param){
-	if(!isset($param['id'])){
-		echo $this->defaut();
-	}else{
-	
-	
-}	
+    if(!isset($param['id'])){
+        echo $this->defaut();
+    }else{
+        $r = new PlatModel() ;
+        $c= $r->getInstance();
+        echo $this->returnJson();
+        echo $c->add($param);
+
+    }
 
 }
 
 public function panier($param){
-	echo Cart::get()->
-		
+    $r = new PlatModel() ;
+    $c= $r->getInstance();
+    echo $this->returnJson();
+    echo $c->get();
 
 }
 
