39feffba2d7163adb3f2b0b2a19b541437be0d94
Nitix <guillaume.pierson.gp@gmail.com>
2014-03-26 20:24:13 +0100

    Quelques javascript

--- js/dracaufeu.js
+++ js/dracaufeu.js
@@ -5,6 +5,12 @@ $(Document).ready(function(){
 
     $(".item").on("click", function(){
         var id = $(this).data('id');
-
+        $.ajax({
+            type : "GET",
+            url : "index.php",
+            data : { "id" : id}
+        }).done(function(res){
+            console.log(res);
+        });
     });
 });
